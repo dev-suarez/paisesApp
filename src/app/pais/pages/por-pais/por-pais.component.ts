49c8e2617088ae03bf0593c2cebb940ddf7d5152
Sergio Suárez <sgabriel4@gmail.com>
2022-07-05 01:13:14 -0400

    Proyecto Terminado

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styleUrls: ['./por-pais.component.css']
+})
+export class PorPaisComponent {
+
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
+
+  constructor(private paisService: PaisService) { }
+
+  buscar(termino: string) {
+    this.mostrarSugerencias = false;
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarPais(this.termino)
+      .subscribe((paises) => {
+        console.log(paises);
+        this.paises = paises;
+      }, (err) => {
+        console.log(err.status);
+        this.hayError = true;
+        this.paises = [];
+      });
+  }
+
+  sugerencias(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+    //TODO: crear sugerencias
+
+    this.paisService.buscarPais(termino)
+      .subscribe(
+        paises => this.paisesSugeridos = paises.splice(0, 5),
+        (err) => this.paisesSugeridos = []
+      );
+  }
+
+  buscarSugerido(termino: string) {
+    this.buscar(termino);
+  }
+
+}
